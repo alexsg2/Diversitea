56055ce147cc646393eb06fc96ca220b8af745d4
alexsg2 <alex.s.georgiev@gmail.com>
2024-02-03 13:14:31 -0500

    NavBar

---
+++ front-end/src/components/NarBar/NavBar.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  const containerStyle = {
+    borderRadius: "15px",
+  };
+
+  const navbarStyle = {
+    width: "80%",
+    display: "flex",
+    justifyContent: "space-around",
+    backgroundColor: "#E0D9F1",
+    padding: "15px",
+  };
+
+  const defaultButtonStyle = {
+    borderRadius: "15px",
+    backgroundColor: "#E0D9F1", // Text color
+    color: "#9384E3", // Button background color
+    border: "none", // Remove border
+  };
+
+  const [buttonStyles, setButtonStyles] = useState({
+    home: defaultButtonStyle,
+    advancedSearch: defaultButtonStyle,
+    help: defaultButtonStyle,
+    compare: defaultButtonStyle,
+  });
+
+  const handleMouseEnter = (buttonKey) => {
+    setButtonStyles({
+      ...buttonStyles,
+      [buttonKey]: {
+        ...defaultButtonStyle,
+        backgroundColor: "#9384E3",
+        color: "#E0D9F1",
+      },
+    });
+  };
+
+  const handleMouseLeave = (buttonKey) => {
+    setButtonStyles({
+      ...buttonStyles,
+      [buttonKey]: defaultButtonStyle,
+    });
+  };
+
+  return (
+    <div className="container-fluid d-flex justify-content-center mt-4" style={containerStyle}>
+      <div className="rounded p-3" style={navbarStyle}>
+        <Link to="/">
+          <button
+            className="btn btn-primary"
+            style={buttonStyles.home}
+            onMouseEnter={() => handleMouseEnter('home')}
+            onMouseLeave={() => handleMouseLeave('home')}
+          >
+            Home
+          </button>
+        </Link>
+        <Link to="/adv-search">
+          <button
+            className="btn btn-primary"
+            style={buttonStyles.advancedSearch}
+            onMouseEnter={() => handleMouseEnter('advancedSearch')}
+            onMouseLeave={() => handleMouseLeave('advancedSearch')}
+          >
+            Advanced Search
+          </button>
+        </Link>
+        <Link to="/help">
+          <button
+            className="btn btn-primary"
+            style={buttonStyles.help}
+            onMouseEnter={() => handleMouseEnter('help')}
+            onMouseLeave={() => handleMouseLeave('help')}
+          >
+            Help
+          </button>
+        </Link>
+        <Link to="/compare">
+          <button
+            className="btn btn-primary"
+            style={buttonStyles.compare}
+            onMouseEnter={() => handleMouseEnter('compare')}
+            onMouseLeave={() => handleMouseLeave('compare')}
+          >
+            Compare
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- front-end/src/webpages/HelpPage/Help.jsx
+++ front-end/src/webpages/HelpPage/Help.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import NavBar from '../../components/NarBar/NavBar';
+
 
 function Home() {
   return (
     <div>
+      <NavBar/>
       <h1>Welcome to the Help Page!</h1>
-      
-      {/* Buttons to navigate to other pages */}
-      <Link to="/adv-search">
-        <button>Advanced Search</button>
-      </Link>
-      
-      <Link to="/help">
-        <button>Help</button>
-      </Link>
-
-      <Link to="/compare">
-        <button>Compare</button>
-      </Link>
     </div>
   );
 }
