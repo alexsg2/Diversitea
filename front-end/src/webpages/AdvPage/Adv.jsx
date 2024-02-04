0a87f355713cc129760b84471951d8477c65509a
Rowan Cusano <rowan.cusano@gmail.com>
2024-02-04 05:38:07 -0500

    leadboard styling

--- front-end/src/webpages/AdvPage/Adv.jsx
+++ front-end/src/webpages/AdvPage/Adv.jsx
@@ -50,107 +50,91 @@ function Adv() {
 
   useEffect(() => {
     // Update the type and companies based on selectedCategory
-    if (selectedCategory === 'Female') {
-      setType('Female');
-      // setCompanies(femaleData.sorted_data);
-    }
-    else if (selectedCategory == "Male") {
-      setType("Male");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "American Indian or Alaskan Native") {
-      setType("American Indian or Alaskan Native");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "Asain") {
-      setType("Asian");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "Black or African American") {
-      setType("Black or African American");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "Hispanic or Latino") {
-      setType("Hispanic or Latino");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "Native Hawaiian or Pacific Islander") {
-      setType("Native Hawaiian or Pacific Islander");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "Two or more races") {
-      setType("Two or more races");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "White") {
-      setType("White");
-      // setCompanies(maleData.sorted_data);
-    }
-    else if (selectedCategory == "Rating") {
-      setType("Rating");
-      // setCompanies(maleData.sorted_data);
-    }
+    // if (selectedCategory === 'Female') {
+    //   setType('Female');
+    //   // setCompanies(femaleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Male") {
+    //   setType("Male");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "American Indian or Alaskan Native") {
+    //   setType("American Indian or Alaskan Native");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Asain") {
+    //   setType("Asian");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Black or African American") {
+    //   setType("Black or African American");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Hispanic or Latino") {
+    //   setType("Hispanic or Latino");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Native Hawaiian or Pacific Islander") {
+    //   setType("Native Hawaiian or Pacific Islander");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Two or more races") {
+    //   setType("Two or more races");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "White") {
+    //   setType("White");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    // else if (selectedCategory == "Rating") {
+    //   setType("Rating");
+    //   // setCompanies(maleData.sorted_data);
+    // }
+    setType(selectedCategory)
 
     // Add conditions for other categories as needed
   }, [selectedCategory]);
 
 
   return (
-    <div>
+    <div className="container">
       <NavBar />
-      <h1>Leader Board</h1>
-      <div style={{
-        margin: '20px 0',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        gap: '10px', // This creates space between buttons without affecting their width
-      }}>
-        {/* Apply a consistent width and margin to each button */}
-        <button style={buttonStyle} onClick={() => handleClick('Female')}>Female</button>
-        <button style={buttonStyle} onClick={() => handleClick('Male')}>Male</button>
-        <button style={buttonStyle} onClick={() => handleClick('American Indian or Alaskan Native')}>American Indian or Alaskan Native</button>
-        <button style={buttonStyle} onClick={() => handleClick('Asian')}>Asian</button>
-        <button style={buttonStyle} onClick={() => handleClick('Black or African American')}>Black or African American</button>
-        <button style={buttonStyle} onClick={() => handleClick('Hispanic or Latino')}>Hispanic or Latino</button>
-        <button style={buttonStyle} onClick={() => handleClick('Native Hawaiian or Pacific Islander')}>Native Hawaiian or Pacific Islander</button>
-        <button style={buttonStyle} onClick={() => handleClick('Two or more races')}>Two or more races</button>
-        <button style={buttonStyle} onClick={() => handleClick('White')}>White</button>
-        <button style={buttonStyle} onClick={() => handleClick('Rating')}>Rating</button>
+      <div style={{ margin: '20px', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '10px' }}>
+        {['Female', 'Male', 'American Indian or Alaskan Native', 'Asian', 'Black or African American', 'Hispanic or Latino', 'Native Hawaiian or Pacific Islander', 'Two or more races', 'White', 'Rating'].map((category, index) => (
+          <button key={index} style={buttonStyle} onClick={() => handleClick(category)}>
+            {category}
+          </button>
+        ))}
       </div>
 
       {type && companies && (
-        <div>
-          <h2>Top Companies for {type} Employee Percentage</h2>
-          <table style={tableStyle}>
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Company Name</th>
-                {type === "Rating" ? <th>Rating</th> : <th>Percentage</th>}
-              </tr>
-            </thead>
-            <tbody>
-              {currentCompanies.map((company, index) => (
-                <tr key={index}>
-                  <td>{index + 1 + (currentPage - 1) * itemsPerPage}</td>
-                  <td>{company.Company}</td>
-                  {type === "Rating" ? <td>{company[`${type}`]}</td> : <td>{company[`${type}`]}%</td>}
+        <div style={{ margin: 'auto', textAlign: 'center', border: '20px', borderColor: 'purple' }}>
+          <h2 style={{ margin: "45px" }}>Top Companies for {type} Employee Percentage</h2>
+          <div style={{ width: '40%', margin: 'auto', alignItems: 'center' }}>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Rank</th>
+                  <th>Company Name</th>
+                  {type === "Rating" ? <th>Rating</th> : <th>Percentage</th>}
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {currentCompanies.map((company, index) => (
+                  <tr key={index}>
+                    <td>{index + 1 + (currentPage - 1) * itemsPerPage}</td>
+                    <td>{company.Company}</td>
+                    {type === "Rating" ? <td>{company[`${type}`]}</td> : <td>{company[`${type}`]}%</td>}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
           <div style={paginationStyle}>
-            {[...Array(Math.ceil(companies.length / itemsPerPage)).keys()].map(number => (
-              <button key={number} onClick={() => paginate(number + 1)} style={pageButtonStyle}>
-                {number + 1}
-              </button>
-            ))}
+            {paginationControls}
           </div>
         </div>
       )}
-
-
     </div>
   );
 }
@@ -163,26 +147,42 @@ const buttonStyle = {
   overflow: 'hidden',
   textOverflow: 'ellipsis',
   whiteSpace: 'nowrap',
-  border: '1px solid #ccc',
-  backgroundColor: '#f9f9f9',
+  backgroundColor: '#7145D9',
   padding: '5px',
   textAlign: 'center',
   cursor: 'pointer', // Changes the cursor on hover to indicate the element is clickable
+  borderRadius: "15px",
+  color: "white",
+  border: "none",
 };
 
-const tableStyle = { width: '100%', textAlign: 'center' };
+const tableStyle = {
+  width: '100%',
+  textAlign: 'center',
+  margin: '5px',
+  border: '20px',
+  borderColor: 'purple',
+  display: 'block', // Set display to block
+  marginLeft: 'auto', // Auto margin from left
+  marginRight: 'auto', // Auto margin from right
+};
+
 
 const paginationStyle = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  padding: '20px',
+  padding: '20px'
 };
 
 const pageButtonStyle = {
   padding: '10px 20px',
   margin: '0 10px',
   cursor: 'pointer',
+  borderRadius: "15px",
+  color: "white",
+  border: "none",
+  backgroundColor: '#7145D9',
 };
 
 
